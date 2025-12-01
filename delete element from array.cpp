6ca6b59f9ad41357f31865a6a64ca5576dd1ab88
madhurimapynni-svg <madhurimapynni@gmail.com>
2025-12-01 21:28:36 +0530

    Add files via upload

---
+++ delete element from array.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, arr[100], pos;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter position to delete: ");
+    scanf("%d", &pos);
+
+    for(int i = pos - 1; i < n - 1; i++)
+        arr[i] = arr[i + 1];
+    n--;
+
+    printf("Array after deletion: ");
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
+
